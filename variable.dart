116f145c73f5a9781063577a00de626d539be2f1
Reuben290 <neequaye.dev@gmail.com>
2026-04-25 21:16:23 +0000

    putting variables  in a string

--- variable.dart
+++ variable.dart
@@ -1,6 +1,12 @@
+//void main(){
+//int something = 2;
+//int other=2;
+//int total=something + other;//total is 4
+//print(total);
+//}
 void main(){
-int something = 2;
-int other=2;
-int total=something + other;//total is 4
-print(total);
+    int temp= 75;
+    String weatherReport="It is rainy and $temp degrees";
+    String obviousReport="if it were 30 degrees cooeler it would be ${temp-30} degrees";
+    print(obviousReport);
 }
