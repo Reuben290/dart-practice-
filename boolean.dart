edf84ea35ffba4d2d597c637e78afc7dcbf77593
Reuben290 <neequaye.dev@gmail.com>
2026-04-28 23:20:28 +0000

    boolean and variable

---
+++ boolean.dart
@@ -0,0 +1,15 @@
+//void main(){
+    ///bool b=true;
+    //if(b){
+    //    print("true");
+  //  }
+//}
+void main(){
+    bool b=true;
+    // ignore: dead_code
+    if (!b){
+        print("true");
+    } else{
+        print("false");
+    }
+}
--- variable.dart
+++ variable.dart
@@ -4,9 +4,12 @@
 //int total=something + other;//total is 4
 //print(total);
 //}
+//void main(){
+  //  int temp= 75;
+    //String weatherReport="It is rainy and $temp degrees";
+    //String obviousReport="if it were 30 degrees cooeler it would be ${temp-30} degrees";
+    //print(obviousReport);
+//}
 void main(){
-    int temp= 75;
-    String weatherReport="It is rainy and $temp degrees";
-    String obviousReport="if it were 30 degrees cooeler it would be ${temp-30} degrees";
-    print(obviousReport);
+    print("\"I am very tired,\"she said");
 }
